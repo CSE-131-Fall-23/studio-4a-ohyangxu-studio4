5902dd8b2b7e4204ff9a764834023bb9b2716d48
Daniel Oh <Daniel Oh@DanielO_o.dhcp.wustl.edu>
2023-10-04 13:52:24 -0500

    Example rectangle

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,14 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setXscale(0, 10);
+		StdDraw.setYscale(0, 10);
+		
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.line(0.5, 0.5, 2.5, 0.5);
+		StdDraw.line(0.5, 0.5, 0.5, 0.7);
+		StdDraw.line(2.5, 0.7, 0.5, 0.7);
+		StdDraw.line(2.5, 0.7, 2.5, 0.5);
+
 	}
 }
